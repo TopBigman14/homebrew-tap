f1539710de086a9f74e95d14650a7add60364c36
TopBigman14 <topbigman14@duck.com>
2026-07-03 20:35:53 -0400

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,4 +1,4 @@
-class Myscript < Formula
+class Test < Formula
   desc "My Bash script"
   homepage "https://github.com/TopBigman14/projects"
   url "https://github.com/TopBigman14/projects/archive/refs/tags/v1.0.0.tar.gz"
