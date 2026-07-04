fe71a122119b3f9c2dd8dae6fc1fdd7001449ac7
TopBigman14 <topbigman14@duck.com>
2026-07-03 20:46:55 -0400

    Update testy.rb

--- Formula/testy.rb
+++ Formula/testy.rb
@@ -2,7 +2,7 @@ class Testy < Formula
   desc "My Bash script"
   homepage "https://github.com/TopBigman14/projects"
   url "https://github.com/TopBigman14/projects/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "c6ca21335fa9c5b5bfc0ce267bf3c89364005d06ab33dc39c79964ca00bdf116"
+  sha256 "c168761f9c697b7726617403acc9c2cb69829e75c9964f4c4a8a282c6bdb0d9d"
 
   def install
     bin.install "bin/testy"
