5077a43b0da1e5c0248d262903d9e59fa262df5b
TopBigman14 <topbigman14@duck.com>
2026-07-03 20:26:27 -0400

    Create test.rb

---
+++ Formula/test.rb
@@ -0,0 +1,10 @@
+class Myscript < Formula
+  desc "My Bash script"
+  homepage "https://github.com/TopBigman14/projects"
+  url "https://github.com/TopBigman14/projects/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "..."
+
+  def install
+    bin.install "bash/myscript"
+  end
+end
