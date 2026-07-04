0cfe4cb2e94c0d6b560fadd4b5e637c57acec8cf
TopBigman14 <topbigman14@duck.com>
2026-07-03 20:40:27 -0400

    Update test.rb

--- Formula/test.rb
+++ Formula/test.rb
@@ -2,7 +2,7 @@ class Test < Formula
   desc "My Bash script"
   homepage "https://github.com/TopBigman14/projects"
   url "https://github.com/TopBigman14/projects/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "..."
+  sha256 "c6ca21335fa9c5b5bfc0ce267bf3c89364005d06ab33dc39c79964ca00bdf116"
 
   def install
     bin.install "bin/test"
